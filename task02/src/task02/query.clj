fd23c9e3f8177d77881a55e27f4068ce75a2ffc0
Reiver <hlam-box@yandex.ru>
2014-03-28 10:07:34 +0400

    Task02 done

---
+++ task02/src/task02/query.clj
@@ -0,0 +1,88 @@
+(ns task02.query
+  (:use [task02 helpers db])
+  (require [clojure.core.match :as cm]))
+
+(def operators (zipmap '("=" "!=" "<" ">" "<=" ">=") (list = not= < > <= >=)))
+
+(defn make-where-value [val]
+  (let [value (re-matches #"'(.*)'" val)]
+    (if value
+      (second value)
+      (parse-int val))))
+
+(defn make-where-function [field op val]
+  (let [value (make-where-value val)
+        fk (keyword field)
+        func (get operators op)]
+     (fn [data]
+       (func (fk data) value))))
+
+(defn parse-select-impl [query]
+  (cm/match [query]
+            [["select" table & rest]] (list (list table) rest)
+            [["where" field op val & rest]] (list (list :where (make-where-function field op val)) rest)
+            [["order" "by" column & rest]] (list (list :order-by (keyword column)) rest)
+            [["limit" val & rest]] (list (list :limit (parse-int val)) rest)
+            [["join" table "on" col1 "=" col2 & rest]] (list (list :joins [[(keyword col1) table (keyword col2)]]) rest) 
+            :else (list nil [])))
+
+;; Функция выполняющая парсинг запроса переданного пользователем
+;;
+;; Синтаксис запроса:
+;; SELECT table_name [WHERE column comp-op value] [ORDER BY column] [LIMIT N] [JOIN other_table ON left_column = right_column]
+;;
+;; - Имена колонок указываются в запросе как обычные имена, а не в виде keywords. В
+;;   результате необходимо преобразовать в keywords
+;; - Поддерживаемые операторы WHERE: =, !=, <, >, <=, >=
+;; - Имя таблицы в JOIN указывается в виде строки - оно будет передано функции get-table для получения объекта
+;; - Значение value может быть либо числом, либо строкой в одинарных кавычках ('test').
+;;   Необходимо вернуть данные соответствующего типа, удалив одинарные кавычки для строки.
+;;
+;; - Ключевые слова --> case-insensitive
+;;
+;; Функция должна вернуть последовательность со следующей структурой:
+;;  - имя таблицы в виде строки
+;;  - остальные параметры которые будут переданы select
+;;
+;; Если запрос нельзя распарсить, то вернуть nil
+
+;; Примеры вызова:
+;; > (parse-select "select student")
+;; ("student")
+;; > (parse-select "select student where id = 10")
+;; ("student" :where #<function>)
+;; > (parse-select "select student where id = 10 limit 2")
+;; ("student" :where #<function> :limit 2)
+;; > (parse-select "select student where id = 10 order by id limit 2")
+;; ("student" :where #<function> :order-by :id :limit 2)
+;; > (parse-select "select student where id = 10 order by id limit 2 join subject on id = sid")
+;; ("student" :where #<function> :order-by :id :limit 2 :joins [[:id "subject" :sid]])
+;; > (parse-select "werfwefw")
+;; nil
+(defn parse-select [^String sel-string]
+  (loop [input (vec (.split sel-string " "))
+        query []]
+    (if (empty? input)
+      (let [res (apply concat query)]
+        ;; Если получается пустой список - вернуть nil
+        (if (empty? res) nil
+            res))
+      (let [r (parse-select-impl input)]
+        (recur (second r)
+               (conj query (first r)))))))
+
+;; Выполняет запрос переданный в строке.  Бросает исключение если не удалось распарсить запрос
+
+;; Примеры вызова:
+;; > (perform-query "select student")
+;; ({:id 1, :year 1998, :surname "Ivanov"} {:id 2, :year 1997, :surname "Petrov"} {:id 3, :year 1996, :surname "Sidorov"})
+;; > (perform-query "select student order by year")
+;; ({:id 3, :year 1996, :surname "Sidorov"} {:id 2, :year 1997, :surname "Petrov"} {:id 1, :year 1998, :surname "Ivanov"})
+;; > (perform-query "select student where id > 1")
+;; ({:id 2, :year 1997, :surname "Petrov"} {:id 3, :year 1996, :surname "Sidorov"})
+;; > (perform-query "not valid")
+;; exception...
+(defn perform-query [^String sel-string]
+  (if-let [query (parse-select sel-string)]
+    (apply select (get-table (first query)) (rest query))
+    (throw (IllegalArgumentException. (str "Can't parse query: " sel-string)))))
